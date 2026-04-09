858ccf7952907c03838bc4cfceb113daab61007b
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-09 14:59:25 +0500

    Add countPositivesSumNegatives function
    Implement function to count positives and sum negatives

---
+++ app.js
@@ -0,0 +1,19 @@
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length == 0) {
+    return [];
+  }
+
+  let positives = 0;
+  let negatives = 0;
+
+  for (el of input) {
+    if (el > 0) {
+      positives += 1;
+    } else {
+      negatives += key;
+    }
+  }
+
+  return [positives, negatives];
+}
+countPositivesSumNegatives([1, 2, 3, -4, -5, 6, -7, 8]);
